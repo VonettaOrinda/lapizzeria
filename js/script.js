7d5edad4afdaccd0cff96027addab3a686e3a91a
VONETTA <thevonetteway@gmail.com>
2022-03-21 07:38:42 +0300

    add variable,pizza,toppings,crust and flavor

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ function capitalize_inputs(str) { //function to capitalize inputs
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
   }
+
+  function placeOrder() { //function for placing an order
+    //get inputs from form
+    var name = capitalize_inputs($("input#name").val());
+    var flavor = $("#pizza-flavor").val();
+    var size = $("#pizza-size").val();
+    var crust = $("#pizza-crust").val();
+    var toppings = [];
+    $.each($('input[name="toppings"]:checked'),
+        function() {
+            toppings.push($(this).val());
+        });
+        
